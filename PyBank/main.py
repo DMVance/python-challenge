4bf0928fefa369b6f01f1f6cc9c6f21b221dc2b5
DMVance <csljrrt@gmail.com>
2020-08-28 14:00:09 -0500

    add csv.DictReader, prep for operations

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,16 @@
 import os, csv
 
-path = os.path.join('Resources', 'budget_data.csv')
-output_path = os.path.join('analysis', 'analysis.txt')
+path = os.path.join("Resources", "budget_data.csv")
+output_path = os.path.join("analysis", "analysis.txt")
 
-with open(path, "r") as file:
+with open(path, "r") as in_file:
 
+    # data has headers, so use csv.DictReader(file)
+    csv_reader = csv.DictReader(in_file)
+    data = list(csv_reader)
 
+    # Next, iterate over contents of file, line by line, and perform appropriate operation to obtain the desired outcomes listed below
+    
 
     #total_months = a count of the mumber of rows
     #net_total_profit_losses = sum of Profit/Losses column
@@ -13,18 +18,20 @@ with open(path, "r") as file:
     #greatest_increase_profits_(date/amount over entire period) = max of new column, provide month and value
     #greatest_decrease_losses_(date/amount over entire period) = min of new column, provide month and value
 
-with open(path, "w+") as file:
+print(data)
 
-    # establish name of output file?
-
-    print("Financial Analysis\n")
-    print("-------")
-    print(f"Total Months: {total_months}\n")
-    print(f"Total: ${total}\n")
-    print(f"Average Change: ${avg_change}\n")
-    print(f"Greatest Increase in Profits: {date} (${greatest_increase_profits})\n")
-    print(f"Greatest Decrease in Profits: {date} (${greatest_decrease_losses})\n")
-
-    csv.writer(txtfile)
-
-    print(analysis)
+#with open(path, "w+") as out_file:
+#
+#    # establish name of output file?
+#
+#    print("Financial Analysis\n")
+#    print("-------")
+#    print(f"Total Months: {total_months}\n")
+#    print(f"Total: ${total}\n")
+#    print(f"Average Change: ${avg_change}\n")
+#    print(f"Greatest Increase in Profits: {date} (${greatest_increase_profits})\n")
+#    print(f"Greatest Decrease in Profits: {date} (${greatest_decrease_losses})\n")
+#
+#    csv.writer(txtfile)
+#
+#    print(analysis)
