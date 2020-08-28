7c358e43e9bc4ee46ea2624908fe78bf5c5e5174
David Vance <davidvance@Davids-iMac.attlocal.net>
2020-08-27 23:44:11 -0500

    added scaffolding

---
+++ PyBank/main.py
@@ -0,0 +1,15 @@
+import os, csv
+
+path = os.path.join('Resources', 'budget_data.csv')
+
+with open(path, "r") as file:
+
+    num_months
+    net_total_profit_losses
+    avg_changes_profit_losses
+    greatest_increase_profits_(date/amount over entire period)
+    greatest_decrease_losses_(date/amount over entire period)
+
+
+print(analysis)
+csv.writer(txtfile)
