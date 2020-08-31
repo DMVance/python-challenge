6d4d5e6ab180d98703fb5fec7c8f1d72b1d7096c
csljrrt <jupyter@pythondv.us-west1-b.c.still-motif-285115.internal>
2020-08-31 03:34:13 +0000

    combine total mos and total profit code blocks

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # This program creates a Python script for analyzing the financial records of your company.
 
-import os, csv, pprint
+import os, csv
 
 path = os.path.join("Resources", "budget_data.csv")
 output_path = os.path.join("analysis", "analysis.txt")
@@ -9,29 +9,51 @@ output_path = os.path.join("analysis", "analysis.txt")
 print("Financial Analysis")
 print("---------------------------") # is there a report formatting way to do this?
 
-# use DictReader from csv module to take each row and move into a list of dictionaries (rather than a list of lists), preserving header info for each line
-with open(path, "r") as file:
-    
-    dict_reader = csv.DictReader(file)
-    #pprint.pprint([dict(ordered_dict) for ordered_dict in dict_reader])
-        
-    tot_months = 0           # Improve with a list comprehension?
-    for line in dict_reader: # if i had used file instead of dict_reader, it would have counted the header line
-        tot_months += 1
-    print(f"Total months: {tot_months} \n")
-
 with open(path, "r") as file:
 
     data = csv.reader(file)
 
     next(file)
     
-    total = 0  # Improve with a list comprehension?
-    for line in data:
-        #print(line[1])
+    tot_months = 0    # Improve with a list comprehension?
+    total = 0 
+    for line in data: # if i had used file instead of dict_reader, it would have counted the header line
+        tot_months += 1
         total += int(line[1])
+    print(f"Total months: {tot_months}")
+    print(f"Total: ${total}")
     
-print(f"Total: ${total}")
+with open(path, "r") as file:
+   
+    data = dict(csv.DictReader(file))
+    avg_change = 0          #change / tot
+
+    #iterate over file: subtract second item from first item
+    #take avg and print result
+    tot = 0 
+    change = [profit[date + 1] - profit[date] for date, profit in file]
+    for line in data:
+        tot += 1
+        profit = int(data[line]["Profit/Losses"]
+#         print(profit)
+        
+#     print(f"Average Change: ${avg_change}")
+
+with open(path, "r") as file:
+
+    greatest_incr = 0
+    
+    
+    
+    print(f"Greatest Increase in Profits: ${greatest_incr}")
+    
+with open(path, "r") as file:
+
+    greatest_decr = 0
+    
+    
+    
+    print(f"Greatest Decrease in Profits: ${greatest_decr}")
     
     # Iterate over contents of file, line by line, and perform appropriate operation to obtain the desired outcomes listed below
     #months = 0
